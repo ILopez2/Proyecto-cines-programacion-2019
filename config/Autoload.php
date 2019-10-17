5659a8fa3f773e320e0abf92bb517c149cbcacc7
Ivan Lopez <48885606+ILopez2@users.noreply.github.com>
2019-10-17 13:49:27 -0300

    Se arregla autoload

--- config/Autoload.php
+++ config/Autoload.php
@@ -4,12 +4,7 @@ class Autoload {
      public static function start() {
           spl_autoload_register(function($class)
           {
-               // Armo la url de la clase a partir del namespace y la instancia.
                echo $url = ROOT . '/' . str_replace("\\", "/", $class)  . ".php";
-               // Convierto la url a minúsculas ya que mi estructura de directorios y ficheros esta toda en minúsculas
-               // echo $url = strtolower($url);
-               echo '<br>';
-               // Incluyo la url que, si todo esta bien, debería contener la clase que intento instanciar.
                include_once($url);
           });
      }
