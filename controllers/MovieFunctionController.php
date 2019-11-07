814319c760fd845e3ca8c2c4bd8600f057a16f86
ivanmdq22 <ivangraciarena@outlook.com>
2019-11-07 09:00:28 -0300

    agrego controller defunciones

---
+++ controllers/MovieFunctionController.php
@@ -0,0 +1,48 @@
+<?php namespace controllers;
+    
+    use dao\MovieFunctionDao as MovieFunctionDao;
+    use models\ClassCinema as Cinema;
+    use models\ClassCinemaRoom as ClassCinemaRoom;
+    use controllers\ViewsController as View;
+    use models\ClassMovieFunction as ClassMovieFunction;
+    
+    class MovieFunctionController{
+        
+        private $dao;
+        private $view
+
+        public function __construct(){
+            //$this->cinemaDao = new JsonCinema();
+            $this->dao = new MovieFunctionDao();
+            $this->view = new View();
+        }
+
+
+        public function add($movie,$cinema,$dateTime,$language,$cinemaRoom,$id){
+            if(isset($_SESSION['loggedRole']) && $_SESSION['loggedRole'] == '1'){
+                $function = new ClassMovieFunction($movie,$cinema,$dateTime,$language,$cinemaRoom,$id);
+                $this->dao->add($function);
+                $this->view->admFunctions();
+            }
+
+        }
+
+
+        public function delete($id){
+            //borrar el cine con el nombre pasado por parameto
+            if(isset($_SESSION['loggedRole']) && $_SESSION['loggedRole'] == '1'){
+                $this->dao->delete($id);
+                $this->view->admFunctions();
+            }
+        }
+
+        public function edit($movie,$cinema,$dateTime,$language,$cinemaRoom,$id){
+            //editar el valor del atributo seleccionado por el usuario del cine seleccionado
+            if(isset($_SESSION['loggedRole']) && $_SESSION['loggedRole'] == '1'){
+                $function = new ClassMovieFunction($movie,$cinema,$dateTime,$language,$cinemaRoom,$id);
+                $this->cinemaDao->modify($function);
+                $this->view->admFunctions();
+            }
+        }
+
+    }
