7314a7cbc8c2209065df00722dbe792620666a16
Ivan Lopez <48885606+ILopez2@users.noreply.github.com>
2019-11-25 13:00:07 -0300

    Se continua con el envio del email ahora con php mailer

--- controllers/EmailController.php
+++ controllers/EmailController.php
@@ -5,13 +5,34 @@
     use dao\MovieFunctionDao as MovieFunctionDao;
     use dao\CinemaDao as CinemaDao;
     use dao\MovieDao as MovieDao;
+    use PHPMailer\PHPMailer\PHPMailer;
+    use PHPMailer\PHPMailer\Exception;
+    
+    require 'PHPMailer/src/Exception.php';
+    require 'PHPMailer/src/PHPMailer.php';
+    require 'PHPMailer/src/SMTP.php';
 
     class EmailController{
 
         private $from;
+        private $pass;
+        private $mailSender;
 
         public function __construct(){
-            $from = "themoviepassoriginal@gmail.com";
+            $this->from = "themoviepassoriginal@gmail.com";
+            $this->pass= "moviepass123";
+            $this->mailSender = new PHPMailer();
+            $this->mailSender->IsSMTP();
+            $this->mailSender->SMTPAuth = true;
+            $this->mailSender->Host = "smtp.gmail.com"; // SMTP a utilizar. Por ej. smtp.elserver.com
+            $this->mailSender->Username =$this->from; // Correo completo a utilizar
+            $this->mailSender->Password =$this->pass; // Contraseña
+            $this->mailSender->Port = 587; // Puerto a utilizar
+            //Con estas pocas líneas iniciamos una conexión con el SMTP. Lo que ahora deberíamos hacer, es configurar el mensaje a enviar, el //From, etc.
+            // Desde donde enviamos (Para mostrar)
+            $this->mailSender->From = $this->from; 
+            $this->mailSender->FromName = "Movie Pass";
+            $this->mailSender->IsHTML(true);
         }
 
         public function sendTickets($qrs,$userId,$functionId){
@@ -30,12 +51,15 @@
             $functionTime=$function->getTime();
             $qrImages="";
             foreach($qrs as $qr){
-                $qrImages.="\r\n<img src=".$qr."/>";
+                $qrImages.="<br><img src=".$qr."/>";
             }
             $to=$userEmail;
+            $message="Presente estos codigos qr en el cine ".$cinemaName." para poder acceder a la funcion de ".$movieTitle." el dia ".$functionDate." en el horario ".$functionTime."<br>-Entradas: ".$qrImages;
             $subject="MoviePass Entradas para ver ".$movieTitle;
-            $headers="De: ".$this->from."\r\n";
-            $message="Presente estos codigos qr en el cine ".$cinemaName." para poder acceder a la funcion de ".$movieTitle." el dia ".$functionDate." en el horario ".$functionTime."\r\n-Entradas: ".$qrImages;
-            mail($to,$subject,$message, $headers);
+            $header="De: ".$this->from."<br>";
+            $this->mailSender->Subject = $header;
+            $this->mailSender->AddAddress($to);
+            $this->mailSender->Body=$message;
+            $this->mailSender->send();
         }
     }
